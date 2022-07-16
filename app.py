430277d294bc1330792a208ad0de0dcd626c4bdb
MaciejKarasek <maciejkarasekk@gmail.com>
2022-07-16 23:25:42 +0200

    Added counting points

--- app.py
+++ app.py
@@ -1,23 +1,39 @@
-from flask import Flask, redirect, render_template, request, flash
+from flask import Flask, redirect, render_template, request, flash, session
+from flask_session import Session
 from game import RPS
 app = Flask(__name__)
 app.secret_key = 'aAbBcD'
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    color = 0
     if request.method == "POST":
         pchoice = request.form['button']
+        if pchoice == "clear":
+            session.clear()
+            flash("Points cleared", 'clear')
+            return redirect("/")
+
         if not pchoice in ['R', 'P', 'S']:
-            flash("WRONG INPUT!", lost)
+            flash("WRONG INPUT!", 'lost')
             return redirect("/")
         else:
             status, bchoice = RPS(pchoice)
-            color = status
+            P = int(session.get('P'))
+            B = int(session.get('B'))
+            P += {0:0, 1:1, 2:0}[status]
+            B += {0:1, 1:0, 2:0}[status]
+            session['P'] = P
+            session['B'] = B
             bottxt = {'R':'Rock', 'P':'Paper', 'S':'Scissors'}[bchoice]
             resulttxt = {0:'lost', 1:'won', 2:'tied'}[status]
             result = "Bot choice is {}, you {}!".format(bottxt, resulttxt)
             flash(result, resulttxt)
             return redirect("/")
     else:
-        return render_template("index.html")
+        if not session.get('B'):
+            session['B'] = 0
+            session['P'] = 0
+        return render_template("index.html", bot = session.get('B'), player = session.get('P'))
