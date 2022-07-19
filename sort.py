4bd2a6a0d49da22043537ab73c0ce1b0087da167
MaciejKarasek <maciejkarasekk@gmail.com>
2022-07-19 15:05:49 +0200

    added quick-sort algorithm

--- sort.py
+++ sort.py
@@ -1,4 +1,4 @@
-from numpy import zeros, random
+from numpy import random
 
 def mergesort(arr):
     if len(arr) > 1:
@@ -37,23 +37,40 @@ def insertsort(arr):
     for i in range(1,len(arr)):
         for j in range(i):
             if arr[i] < arr[j]:
-                tmp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = tmp
+                arr[i], arr[j] = arr[j], arr[i]
     return arr
 
 def bubblesort(arr):
     for i in range(len(arr)):
         for j in range(len(arr)-1):
             if arr[j] > arr[j+1]:
-                tmp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = tmp
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
+def quicksort(arr, low, high):
+    if low < high:
+
+        pi = part(arr, low, high)
+        quicksort(arr, low, pi - 1)
+        quicksort(arr, pi + 1, high)
+
+
+def part(arr, low, high):
+    pivot = arr[high]
+
+    n = low - 1
+    for i in range(low, high):
+        if arr[i] <= pivot:
+            n += 1
+            arr[n], arr[i] = arr[i], arr[n]
+    
+    arr[n + 1], arr[high] = arr[high], arr[n + 1]
+
+    return n + 1
+
 if __name__ == "__main__":
-    arr = random.randint(1, 10000, 5000)
+    arr = random.randint(1, 10000, 10000)
     #arr = [5, 3, 4, 2, 1]
     print(arr)
-    x = mergesort(arr)
-    print(x)
+    quicksort(arr, 0, len(arr) - 1)
+    print(arr)
